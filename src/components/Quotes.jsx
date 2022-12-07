a49554172dd0be1a4be8c670690da35997812305
Aljoscha <joschidev@gmail.com>
2022-12-07 17:52:22 +0100

    small changes

---
+++ src/components/Quotes.jsx
@@ -0,0 +1,23 @@
+import { React } from "react";
+
+const Quotes = () => {
+    return (
+        <div id="quote-box">
+            <div id="image">
+                <img src="#" alt="" id="img" />
+            </div>
+            <div id="text">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur alias pariatur nemo nesciunt, itaque accusamus maiores veniam! Similique, laborum. Reprehenderit odit cumque accusamus facilis repellat voluptate eligendi soluta eos delectus!
+                </p>
+            </div>
+            <div id="author">
+                <p>
+                    John Doe
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Quotes;
